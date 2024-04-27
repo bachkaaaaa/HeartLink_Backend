7ddafd4c446debe5565f1ec1d906a460bdc63bd7
bachakMedAmin <bachakmohamed1@gmail.com>
2024-04-27 23:26:27 +0100

    testing render deploy

--- src/main/java/ensitpfa/backendhl/Config/SecurityConfiguration.java
+++ src/main/java/ensitpfa/backendhl/Config/SecurityConfiguration.java
@@ -34,7 +34,7 @@ public class SecurityConfiguration {
 
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/**")
-                        .permitAll().anyRequest().authenticated()
+                        .permitAll().anyRequest().permitAll()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
